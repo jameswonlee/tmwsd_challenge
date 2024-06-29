dea589dc15d5656b7409b3db3907dfa978cc294d
James Lee <jamesleex86@gmail.com>
2024-06-29 10:04:19 -0700

    Create sqlite3 database

---
+++ db/db.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./messages.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the messages database');
+
+        db.run(`CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            message TEXT NOT NULL
+        )`);
+    }
+});
