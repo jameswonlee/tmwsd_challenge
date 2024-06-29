5796258cb6d853494fed86259353391fe57736f3
James Lee <jamesleex86@gmail.com>
2024-06-29 14:18:26 -0700

    Add setTimeout to single message query route - not working

--- routes/messages.js
+++ routes/messages.js
@@ -17,19 +17,34 @@ router.get('/', function (req, res) {
 router.get('/message/:id', (req, res) => {
   const id = req.params.id;
 
-  db.get('SELECT message FROM messages WHERE id = ?', [id], (err, row) => {
+  db.get('SELECT message FROM messages WHERE id = (?)', [id], (err, row) => {
     if (err) {
-      return console.error(err.message, 'here');
+      return console.error(err.message);
     }
     res.render('messages/show', { message: row });
+
+    setTimeout(() => {
+      db.run('DELETE FROM messages WHERE id = (?)', [id], (err) => {
+        if (err) {
+          return console.error(err.message)
+        }
+        console.log(`Message with id: ${id} deleted from database`)
+      })
+      res.redirect('/')
+    }, 5000)
   })
 })
 
+// Creates a message
+router.post('/create', (req, res) => {
+  const message = req.body.content;
 
-
-
-// router.post('/create', (req, res) => {
-//   const message = req.body.content;
-// });
+  db.run('INSERT INTO messages (message) VALUES (?)', [message], (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect('/');
+  })
+});
 
 module.exports = router
